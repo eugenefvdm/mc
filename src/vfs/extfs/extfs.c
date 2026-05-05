eaa2d8ecbe6d2d2b15560b8947ea45ace780b95d
Yury V. Zaytsev <yury@shurup.com>
2026-05-05 10:40:07 +0200

    (extfs_find_entry_int): fix build due to C23 `strchr` prototype change
    ```
    extfs.c: In function 'extfs_find_entry_int':
    extfs.c:254:11: error: assignment discards 'const' qualifier from pointer target type [-Werror=discarded-qualifiers]
      254 |         q = strchr (p, PATH_SEP);
          |           ^
    ```
    
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- src/vfs/extfs/extfs.c
+++ src/vfs/extfs/extfs.c
@@ -232,9 +232,9 @@ static struct vfs_s_entry *
 extfs_find_entry_int (struct vfs_s_inode *dir, const char *name, GSList *list, int flags)
 {
     struct vfs_s_entry *pent, *pdir;
-    const char *p, *name_end;
-    char *q;
     char c = PATH_SEP;
+    char *name_copy;
+    const char *p, *name_end;
     struct extfs_super_t *super;
 
     if (g_path_is_absolute (name))
@@ -246,17 +246,18 @@ extfs_find_entry_int (struct vfs_s_inode *dir, const char *name, GSList *list, i
 
     super = EXTFS_SUPER (dir->super);
     pent = dir->ent;
-    p = name;
-    name_end = name + strlen (name);
+    name_copy = g_strdup (name);
+    p = name_copy;
+    name_end = name_copy + strlen (name_copy);
 
     while ((pent != NULL) && (c != '\0') && (*p != '\0'))
     {
-        q = strchr (p, PATH_SEP);
+        const char *q = strchr (p, PATH_SEP);
         if (q == NULL)
-            q = (char *) name_end;
+            q = name_end;
 
         c = *q;
-        *q = '\0';
+        name_copy[q - name_copy] = '\0';
 
         if (DIR_IS_DOTDOT (p))
             pent = pent->dir != NULL ? pent->dir->ent : NULL;
@@ -267,14 +268,14 @@ extfs_find_entry_int (struct vfs_s_inode *dir, const char *name, GSList *list, i
             pent = extfs_resolve_symlinks_int (pent, list);
             if (pent == NULL)
             {
-                *q = c;
+                g_free (name_copy);
                 return NULL;
             }
 
             if (!S_ISDIR (pent->ino->st.st_mode))
             {
-                *q = c;
                 notadir = TRUE;
+                g_free (name_copy);
                 return NULL;
             }
 
@@ -283,9 +284,9 @@ extfs_find_entry_int (struct vfs_s_inode *dir, const char *name, GSList *list, i
             pent = pl != NULL ? VFS_ENTRY (pl->data) : NULL;
             if (pent != NULL && q + 1 > name_end)
             {
-                // Hack: I keep the original semanthic unless q+1 would break in the strchr
-                *q = c;
+                // Hack: I keep the original semantic unless q+1 would break in the strchr
                 notadir = !S_ISDIR (pent->ino->st.st_mode);
+                g_free (name_copy);
                 return pent;
             }
 
@@ -297,12 +298,16 @@ extfs_find_entry_int (struct vfs_s_inode *dir, const char *name, GSList *list, i
         }
 
         // Next iteration
-        *q = c;
+        name_copy[q - name_copy] = c;
         if (c != '\0')
             p = q + 1;
     }
+
+    g_free (name_copy);
+
     if (pent == NULL)
         my_errno = ENOENT;
+
     return pent;
 }
 
